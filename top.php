4ae5ff32b9c0e3ebb9dac0bc93069cfd8c8ea64b
Yash Desai <yddc902@live.com>
2020-05-29 18:50:54 +0530

    Create top.php

---
+++ top.php
@@ -0,0 +1,12 @@
+<?php include 'header.php' ?>
+<body>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="signup.php">Sign-Up</a></li>
+			<li><a href="login.php">Login</a></li>
+		</ul>
+</nav>
+<div class="container">
